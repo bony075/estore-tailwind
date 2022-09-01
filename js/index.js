af26a9a03a0833813f82d6425f35a6224a2fb6b1
Bony <bony.075@gmail.com>
2022-09-02 03:40:17 +0600

    dynamic search done

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const setAllMenu = async () => {
 
         if (uniqueArray.indexOf(product.category) === -1) {
             uniqueArray.push(product.category);
-            console.log(product.category);
+            // console.log(product.category);
             const li = document.createElement('li');
             li.innerHTML = `<a>${product.category}</a>`;
             menu.appendChild(li);
@@ -21,3 +21,32 @@ const setAllMenu = async () => {
     }
 }
 setAllMenu();
+const searchField = document.getElementById('search-field');
+searchField.addEventListener("keypress", async (event) => {
+    if (event.key === "Enter") {
+        // console.log(event.key);
+        const searchValue = searchField.value;
+        const allProducts = await loadAllProducts(searchValue);
+        // console.log(allProducts);
+        const foundProducts = allProducts.filter(product => product.category.includes(searchValue));
+        // console.log(foundProducts);
+        const productContainer = document.getElementById('products-container');
+        productContainer.textContent = '';
+        foundProducts.forEach(product => {
+            const { image, title, category } = product;
+            const div = document.createElement('div');
+            div.innerHTML = `<div class="card card-compact full-width bg-base-100 shadow-xl">
+  <figure><img src="${image}" alt="Shoes" class="h-60 w-full"/></figure>
+  <div class="card-body">
+    <h2 class="card-title">${title.length > 20 ? title.slice(0, 20) + "..." : title}</h2>
+    <p>${category}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>`;
+            productContainer.appendChild(div);
+        });
+
+    }
+});
